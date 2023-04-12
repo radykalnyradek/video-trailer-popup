34239420292d9811737e4e50fba7c775708d831b
Mateusz <matekumateku@gmail.com>
2023-04-12 21:28:06 +0200

    Reduced amount of lines of code

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ const video = document.querySelector("video");
 
 btn.addEventListener("click", () => {
   trailerContainer.classList.add("active");
-  trailerContainer.style.visibility = "visible";
 });
 
 closeIcon.addEventListener("click", () => {
   trailerContainer.classList.remove("active");
-  trailerContainer.style.visibility = "hidden";
   video.pause();
   video.currentTime = 0;
 });
